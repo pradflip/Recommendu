d11b66cab0277e1b0b392cdb17b138ca91eaa2ce
pradflip <feliperprado@outlook.com>
2023-03-26 20:53:30 -0300

    Add project files.

---
+++ RecommenduWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecommenduWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
